40b9413b27156857343d6a6fea0a328a3613dd14
Camden Mac Leod <camden.macleod@pm.me>
2021-05-31 20:43:24 -0400

    Add system tray icon functionality

--- Linux/mainwindow.cpp
+++ Linux/mainwindow.cpp
@@ -1,14 +1,30 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QSystemTrayIcon>
+
+QIcon trayIcon;
+
 MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    mSystemTrayIcon = new QSystemTrayIcon(this);
+    trayIcon = QIcon("://favicon.ico");
+
+
+
+    mSystemTrayIcon->setIcon(trayIcon);
+
+    mSystemTrayIcon->setToolTip("Drink it up!");
+
+    mSystemTrayIcon->setVisible(true);
+
+    setWindowIcon(QIcon("://favicon.ico"));
 }
 
 // On window closing
 MainWindow::~MainWindow() {
-    delete ui;
-
-
+    //delete ui;
+    mSystemTrayIcon->showMessage(tr("Hi"), tr("Test Message"), trayIcon);
 }
 
--- Linux/mainwindow.h
+++ Linux/mainwindow.h
@@ -7,6 +7,8 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+class QSystemTrayIcon;
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -17,5 +19,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QSystemTrayIcon *mSystemTrayIcon;
 };
 #endif // MAINWINDOW_H
